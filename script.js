e44f1d277193321756f00167cc581406346f849a
lokang <lokang@me.com>
2024-06-21 16:24:32 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const targetId = event.target.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
